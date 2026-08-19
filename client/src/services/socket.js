c1ca9cdde71e5bdd94a198ba0b6b2f20bf750334
Ankit Raj <ankit2508raj@gmail.com>
2026-08-20 00:29:57 +0530

    Connect Socket.IO to production backend

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://chatverse-backend-82wy.onrender.com", {
     autoConnect: false,
     transports: ["websocket"],
     reconnection: true,
