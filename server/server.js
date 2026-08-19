0e9c98829ff5742ad52aac3721287ea783d38225
Ankit Raj <ankit2508raj@gmail.com>
2026-08-20 00:26:25 +0530

    Connect frontend to production backend

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ connectDB();    // Connects to MongoDB
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
@@ -32,7 +32,7 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
